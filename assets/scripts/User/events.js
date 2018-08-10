fd9828b018dcdebb8830e1e85c6c4cb9b19595b3
priyanka.harkare@gmail.com <priyanka.harkare@gmail.com>
2018-08-10 08:57:51 -0400

    Remove one console log

--- assets/scripts/User/events.js
+++ assets/scripts/User/events.js
@@ -22,7 +22,6 @@ const onSignIn = function (event) {
 
 const onChangePassword = function (event) {
   event.preventDefault()
-  console.log(`changepw event`)
   const data = getFormFields(event.target)
   authApi.changePassword(data)
     .then(authUi.changePasswordSuccess)
